5d9e0dc77a72f66cb8a9739c11a201ce4e7acfa1
Zaara Khan <45593854+ZaaraKhan@users.noreply.github.com>
2026-03-17 15:53:59 -0700

    fix gallery page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Gallery from "./components/Gallery";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
